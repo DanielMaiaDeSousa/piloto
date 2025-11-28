5effb90726d1b620e2a98423c88fa5c3488ee3a9
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-11-28 17:00:44 -0300

    django4

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path ('', views.index, name="index"),
     path('sobre/', views.sobre, name="sobre"),
+    path('contato/', views.contato, name="contato"),
+    path('ajuda/', views.ajuda, name="ajuda"),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def sobre(request):
     return HttpResponse("Sobre o Sistema Django.")
+
+def contato(request):
+    return HttpResponse("Página de Contato.")
+
+def ajuda(request):
+    return HttpResponse("Página de Ajuda.")
