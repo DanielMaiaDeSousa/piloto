a350b14429af782d0717f99d6d2ea9ba7b440897
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-12-12 00:51:56 -0300

    django13

--- home/urls.py
+++ home/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('ajuda/', views.ajuda, name="ajuda"),
     path('perfil/<str:usuario>/', views.perfil, name='perfil'),
     path('dia/<int:dia>/', views.dia_da_semana, name='dia_da_semana'),
-    path('item/<int:id>/', views.exibir_item, name='exibir_item'),    
+    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    path('produto/', views.produto, name='produto'),    
     path('produto/form/', views.form_produto, name='form_produto'),
 
 ]
